d14110453b286e4ae603bab368ed529b52db0bd0
dotxbrain <infodotxbrain@gmail.com>
2025-07-25 22:08:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,10 +88,11 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/dotxbrain_group')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/dotxbrain_official')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'dotxbrain') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/dotxbrain')
+
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
